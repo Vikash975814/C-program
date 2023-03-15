00c77823a3179bb663f6babbb799218dd9b3a177
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-03-15 23:52:06 +0530

    Create MoveCircle.c

---
+++ Graphical Operations/MoveCircle.c
@@ -0,0 +1,27 @@
+#include<graphics.h>
+#include<conio.h>
+Void main()
+{
+	int gd, gm, i;	
+	char ch;
+
+detectgraph (&gd, &gm);
+initgraph(&gd, &gm);
+
+circle (200,200, 70);
+
+	ch = getch();
+	if(ch == 13)
+	
+circle (250, 200, 70);
+
+getch();
+	}
+	
+
+
+
+
+
+
+
