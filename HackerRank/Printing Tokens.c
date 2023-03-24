f6bb587382336d6891e1cb0ac1a3814f66fbe20a
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-03-24 17:37:15 +0530

    Create Printing Tokens.c

---
+++ HackerRank/Printing Tokens.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+int main()
+{
+    char s[1001];
+    gets(s);
+    for(int i=0;s[i]!='\0';i++)
+    {
+        if(s[i]==' ')
+        printf("\n");
+        else
+         printf("%c",s[i]);
+        }
+        return 0;
+    }
