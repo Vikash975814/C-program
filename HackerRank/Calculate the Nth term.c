4239e22843de9984636e04d9664361c347bf5dbd
vikash Gupta <118985043+Vikash975814@users.noreply.github.com>
2023-03-24 17:46:14 +0530

    Create Calculate the Nth term.c

---
+++ HackerRank/Calculate the Nth term.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int f(int n, int a, int b, int c) {
+    if(n==1)
+   return a;
+   else if(n==2)
+   return b;
+   else if(n==3)
+   return c;
+   else
+    return f(n-1,a,b,c)+f(n-2,a,b,c)+f(n-3,a,b,c);
+}
+int main() {
+    int n, a, b, c; 
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = f(n, a, b, c);
+    printf("%d", ans); 
+    return 0;
+}
